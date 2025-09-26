b8f5f90b9a8ae2284c2df8dd87b3ab531bc0d930
mirko federico <mirkofederico@gmail.com>
2025-09-26 19:42:04 +0200

    Esempi while cicli

---
+++ While/while_ciclo_0_iterazioni.cpp
@@ -0,0 +1,21 @@
+//WHILE ciclo senza iterazione
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int x = 10;
+
+    cout << "Inizio ciclo" << endl;
+
+    while (x < 10)
+    {
+        cout << "x e': " << x << endl;
+        x ++;
+        cout << "x adesso e': " << x << endl;
+    }
+
+    cout << "ciclo finito" << endl;
+}
---
+++ While/while_ciclo_infinito.cpp
@@ -0,0 +1,21 @@
+//WHILE ciclo infinite iterazioni
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int x = 0;
+
+    cout << "Inizio ciclo" << endl;
+
+    while (x < 10)
+    {
+        cout << "x e': " << x << endl;
+
+        cout << "x adesso e': " << x << endl;
+    }
+
+    cout << "ciclo finito" << endl;
+}
